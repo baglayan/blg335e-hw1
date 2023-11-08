a8d8b9149883114ee45b72569c9aef38462c30bb
baglayan <baglayan19@itu.edu.tr>
2023-11-08 16:25:32 +0300

    Create .gitignore

---
+++ QuickSort.cpp
@@ -0,0 +1,15 @@
+// BLG 335E Analysis of Algorithms Project 1
+//
+// Name: Meriç Bağlayan
+// Id  : 150190056
+// Date: 2023-11-08
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 42;
+    cout << "Hello, world! Today's lucky number is " << i << endl;
+    return EXIT_SUCCESS;
+}
